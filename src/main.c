6f126d3e1a287367733ebf03e0fb8b93347898a0
Sim Wei Kang <wksim91@yahoo.com.sg>
2015-03-25 12:11:42 +0800

    Lab session

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 #include "led7seg.h"
 #include "oled.h"
 #include "light.h"
+#include "acc.h"
+#include "temp.h"
+#include "rgb.h"
 
 #include "stdio.h"
 
@@ -18,9 +21,9 @@ volatile uint32_t msTicks = 0;
 volatile uint8_t led7SegFlag = 0;
 volatile uint8_t sampleFlag = 0;
 volatile uint32_t lightVal = 0;
-volatile uint32_t accVal_X = 0;
-volatile uint32_t accVal_Y = 0;
-volatile uint32_t accVal_Z = 0;
+volatile uint8_t accVal_X = 0;
+volatile uint8_t accVal_Y = 0;
+volatile uint8_t accVal_Z = 0;
 volatile uint32_t tempVal = 0;
 
 OPERATING_MODE currentMode = BASIC; //Start in basic mode
@@ -40,11 +43,24 @@ void SysTick_Handler(void) {
 
 /***************	Misc functions	********************/
 void switchMode(void) {
+	if (currentMode == BASIC) {
+		currentMode = RESTRICTED;
+		INDICATOR_BASIC_OFF();
+		INDICATOR_RESTRICTED_ON();
+	} else {
+		currentMode = BASIC;
+		INDICATOR_BASIC_ON();
+		INDICATOR_RESTRICTED_OFF();
+	}
 
 }
 
 void transmit(char* str) {
 
+}
+
+uint32_t getMsTicks(void) {
+	return msTicks;
 }
 
 /***************	Peripheral tasks	******************/
@@ -57,6 +73,7 @@ void led7SegTask(void) {
 void accelerometerTask(void) {
 	switch (currentMode) {
 	case BASIC:
+		acc_read(&accVal_X, &accVal_Y, &accVal_Z);
 
 		break;
 	case RESTRICTED:
@@ -66,11 +83,18 @@ void accelerometerTask(void) {
 }
 
 void lightSensorTask(void) {
+	lightVal = light_read();
 	switch (currentMode) {
 	case BASIC:
-		lightVal = light_read();
+		if (lightVal > FLARE_INTENSITY) {
+			switchMode();
+		}
+
 		break;
 	case RESTRICTED:
+		if (lightVal <= FLARE_INTENSITY) {
+			switchMode();
+		}
 
 		break;
 	}
@@ -79,6 +103,7 @@ void lightSensorTask(void) {
 void tempSensorTask(void) {
 	switch (currentMode) {
 	case BASIC:
+		tempVal = temp_read();
 
 		break;
 	case RESTRICTED:
@@ -95,16 +120,16 @@ void oledTask(void) {
 	switch (currentMode) {
 	case BASIC:
 		sprintf(&data[0][3], "%lu", lightVal);
-		sprintf(&data[1][3], "%lu", tempVal);
-		sprintf(&data[2][3], "%lu", accVal_X);
-		sprintf(&data[3][3], "%lu", accVal_Y);
-		sprintf(&data[4][3], "%lu", accVal_Z);
+		sprintf(&data[1][3], "%.1f", (float) tempVal / 10.0);
+		sprintf(&data[2][3], "%u", accVal_X);
+		sprintf(&data[3][3], "%u", accVal_Y);
+		sprintf(&data[4][3], "%u", accVal_Z);
 		break;
 	case RESTRICTED:
 		//Do nothing, since R is the initialized value
 		break;
 	}
-
+	oled_clearScreen(OLED_COLOR_BLACK);
 	int i;
 	for (i = 0; i < 5; i++) {
 		oled_putString(0, i * OLED_CHAR_HEIGHT, (uint8_t *) data[i],
@@ -142,23 +167,35 @@ void init_spi(void) {
 	SSP_Cmd(LPC_SSP1, ENABLE);
 }
 
-static void i2c_init(void)
-{
-    PINSEL_CFG_Type PinCfg;
+static void i2c_init(void) {
+	PINSEL_CFG_Type PinCfg;
 
-    /* Initialize I2C2 pin connect */
-    PinCfg.Funcnum = 2;
-    PinCfg.Pinnum = 10;
-    PinCfg.Portnum = 0;
-    PINSEL_ConfigPin(&PinCfg);
-    PinCfg.Pinnum = 11;
-    PINSEL_ConfigPin(&PinCfg);
+	/* Initialize I2C2 pin connect */
+	PinCfg.Funcnum = 2;
+	PinCfg.Pinnum = 10;
+	PinCfg.Portnum = 0;
+	PINSEL_ConfigPin(&PinCfg);
+	PinCfg.Pinnum = 11;
+	PINSEL_ConfigPin(&PinCfg);
 
-    // Initialize I2C peripheral
-    I2C_Init(LPC_I2C2, 100000);
+	// Initialize I2C peripheral
+	I2C_Init(LPC_I2C2, 100000);
 
-    /* Enable I2C1 operation */
-    I2C_Cmd(LPC_I2C2, ENABLE);
+	/* Enable I2C1 operation */
+	I2C_Cmd(LPC_I2C2, ENABLE);
+}
+
+static void init_GPIO(void) {
+
+	PINSEL_CFG_Type PinCfg;
+	PinCfg.Funcnum = 0;
+	PinCfg.OpenDrain = 0;
+	PinCfg.Pinmode = 0;
+	PinCfg.Portnum = 0;
+	PinCfg.Pinnum = 2;
+	PINSEL_ConfigPin(&PinCfg);
+	GPIO_SetDir(0, 1 << 2, 0);
+
 }
 
 int main(void) {
@@ -169,13 +206,21 @@ int main(void) {
 
 	init_spi();
 	i2c_init();
+	init_GPIO();
 
 	oled_init();
 	led7seg_init();
+	acc_init();
 	light_init();
+	rgb_init();
+	temp_init(getMsTicks);
 	light_enable();
+	light_setRange(LIGHT_RANGE_4000);
 
 	oled_clearScreen(OLED_COLOR_BLACK);
+
+	INDICATOR_BASIC_ON();
+	INDICATOR_RESTRICTED_OFF();
 
 	while (1) {
 		if (led7SegFlag) {
@@ -186,6 +231,7 @@ int main(void) {
 		if (sampleFlag) {
 			accelerometerTask();
 			lightSensorTask();
+			tempSensorTask();
 			oledTask();
 			sampleFlag = 0;
 		}
